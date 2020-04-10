ff17da54539f9c44e725a7b1428fc48f0cea9cc6
musiccitylove <charlesdpulliam@gmail.com>
2020-04-10 13:10:06 -0500

    adoption page added

---
+++ petful-client-template/src/components/Adoption.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+
+class Adoption extends React.Component{
+    render(){
+        return <h1> Adoption </h1>
+    }
+};
+
+export default Adoption;
--- petful-client-template/src/root/Root.js
+++ petful-client-template/src/root/Root.js
@@ -1,12 +1,14 @@
 import React from 'react';
 import {Route, Switch } from 'react-router-dom';
-import Landing from '../components/Landing'
+import Landing from '../components/Landing';
+import Adoption from '../components/Adoption';
 
 class Root extends React.Component {
     render(){
     return <>
     <Switch>
-    <Route exact path= '/' component = {Landing} />
+    <Route exact path='/' component={Landing} />
+    <Route path='/adoption' component={Adoption} />
     </Switch>
       </>
   }
