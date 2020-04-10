ae135c29175b85adc16088ed0735379feffa8629
Jose <jozylopez27@gmail.com>
2020-04-10 10:46:04 -0500

    start

--- petful-server-template/modules/app/app.js
+++ petful-server-template/modules/app/app.js
@@ -4,6 +4,9 @@ const cors = require('cors')
 const app = express()
 
 app.use(cors())
+app.get('/', (req, res) => {
+    res.send('hello world!')
+})
 
 app.use('/people', require('../people/people.router'))
 app.use('/pets', require('../pets/pets.router'))
